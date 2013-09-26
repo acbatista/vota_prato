9491f50461303d34c8d08cbc89c43738c20093eb
beregueder <beregueder@gmail.com>
2013-09-26 19:42:58 -0300

    Criando a aplicação com Rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = '9e27a5e8091ce5eedd64adce6dedcee8f9c2d5bbb8812ad3e5595113222af980385fed59dc83f4281aa8b0b9213d26e43dda78e65a417c590133dbd67d69be68'
